006086e9b31272f5e65972f3612fcd126167b25d
atari-monk <atari.monk1@gmail.com>
2023-03-10 15:29:33 +0100

    refactor

--- my-templates/title-nav/navView.js
+++ my-templates/title-nav/navView.js
@@ -7,32 +7,43 @@ export class NavView {
   }
 
   createContent(data) {
-    const navTemplate = document.getElementById("template-nav");
-    const nav = navTemplate.content.querySelector(".nav");
+    const nav = this.#createNav(data);
+    this.#createNavLinks(nav, data);
+    this.#createDetectOsCard(nav);
+    document.body.appendChild(nav);
+  }
+
+  #createNav(data) {
+    const template = document.getElementById("template-nav");
+    const nav = template.content.querySelector(".nav");
     let newNav;
     newNav = document.importNode(nav, true);
     const titleEl = newNav.querySelector(".nav-title");
     titleEl.textContent = titleEl.textContent.replace(/{%TITLE%}/g, `${data.title}`);
+    return newNav;
+  }
 
-    const navItemTemplate = document.getElementById("template-nav-item");
-    const navItem = navItemTemplate.content.querySelector(".nav-item");
-    const navItemsEl = newNav.querySelector(".nav-ul");
+  #createNavLinks(nav, data) {
+    const navItems = nav.querySelector(".nav-ul");
+    const template = document.getElementById("template-nav-item");
+    const navItem = template.content.querySelector(".nav-item");
 
     data.nav.forEach(navData => {
       let newNavItem;
       newNavItem = document.importNode(navItem, true);
-      const navEl = newNavItem.querySelector(".nav-item-link");
-      navEl.textContent = navEl.textContent
+      const navItemLink = newNavItem.querySelector(".nav-item-link");
+      navItemLink.textContent = navItemLink.textContent
         .replace(/{%NAV_LINK%}/g, `${navData.title}`);
-      navEl.setAttribute("href", `#${navData.title.toLowerCase()}`);
-      navItemsEl.appendChild(newNavItem);
+      navItemLink.setAttribute("href", `#${navData.title.toLowerCase()}`);
+      navItems.appendChild(newNavItem);
     });
+  }
 
+  #createDetectOsCard(nav) {
     const osCard = new DetectOsCard();
     const osCardEl = osCard.createContent();
-    const navNav = newNav.querySelector("#nav-nav");
+    const navNav = nav.querySelector("#nav-nav");
     navNav.appendChild(osCardEl);
-    document.body.appendChild(newNav);
   }
 }
 
