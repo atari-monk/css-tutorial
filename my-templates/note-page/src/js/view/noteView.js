f2e1a1f6c7cd0b54f299c21d827734e39e0f9d3a
atari-monk <atari.monk1@gmail.com>
2023-03-13 23:44:21 +0100

    aside init

--- my-templates/note-page/src/js/view/noteView.js
+++ my-templates/note-page/src/js/view/noteView.js
@@ -5,15 +5,45 @@ export class NoteView {
     const notes = data.filter(note => note.type === 'note');
     const noteTemplate = document.getElementById("template-note");
     const noteEl = noteTemplate.content.querySelector(".note");
+    const asideTemplate = document.getElementById("template-note-aside");
+    const asideEl = asideTemplate.content.querySelector("aside");
     let newNote, i;
     notes.forEach(note => {
-      newNote = document.importNode(noteEl, true);
-      const titleEl = newNote.querySelector(".note-title");
-      titleEl.textContent = titleEl.textContent.replace(/{%TITLE%}/g, `${note.title}\n`);
-      const noteTextEl = newNote.querySelector(".note-note");
-      noteTextEl.innerHTML = noteTextEl.textContent.replace(/{%NOTE%}/g, note.note.join('\n<br>'));
+      newNote = this.#createNote(newNote, noteEl, note);
+      const textEl = newNote.querySelector(".note-text");
+      if (note.params) {
+        console.log('bum');
+        this.#createAside(asideEl, note, textEl);
+      }
       document.body.appendChild(newNote);
     });
+  }
+
+  #createAside(asideEl, note, textEl) {
+    const newAside = document.importNode(asideEl, true);
+    const parentEl = newAside.querySelector("p");
+    let j = 1;
+    const detail = note.params.map(
+      (param, i) => {
+        const detail = `<mark class="mark-${j}">${param?.desc}</mark>`;
+        j++;
+        if (i > 0 && i % 6 === 0)
+          j = 1;
+        return detail;
+      }
+    )
+      .join("<br>");
+    parentEl.innerHTML = detail;
+    textEl.appendChild(newAside);
+  }
+
+  #createNote(newNote, noteEl, note) {
+    newNote = document.importNode(noteEl, true);
+    const titleEl = newNote.querySelector(".note-title");
+    titleEl.textContent = titleEl.textContent.replace(/{%TITLE%}/g, `${note.title}\n`);
+    const noteTextEl = newNote.querySelector(".note-note");
+    noteTextEl.innerHTML = noteTextEl.textContent.replace(/{%NOTE%}/g, note.note.join('\n<br>'));
+    return newNote;
   }
 }
 
