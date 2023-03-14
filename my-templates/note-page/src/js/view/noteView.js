64487a15409e2362752dbd7fe1fcaf34202477bd
atari-monk <atari.monk1@gmail.com>
2023-03-14 11:02:09 +0100

    marker feature

--- my-templates/note-page/src/js/view/noteView.js
+++ my-templates/note-page/src/js/view/noteView.js
@@ -1,16 +1,16 @@
+import * as tool from './../tool.js';
 
 export class NoteView {
-
   createContent(data) {
-    const notes = data.filter(note => note.type === 'note');
-    const noteTemplate = document.getElementById("template-note");
-    const noteEl = noteTemplate.content.querySelector(".note");
-    const asideTemplate = document.getElementById("template-note-aside");
-    const asideEl = asideTemplate.content.querySelector("aside");
+    const notes = data.filter((note) => note.type === 'note');
+    const noteTemplate = document.getElementById('template-note');
+    const noteEl = noteTemplate.content.querySelector('.note');
+    const asideTemplate = document.getElementById('template-note-aside');
+    const asideEl = asideTemplate.content.querySelector('aside');
     let newNote, i;
-    notes.forEach(note => {
+    notes.forEach((note) => {
       newNote = this.#createNote(newNote, noteEl, note);
-      const textEl = newNote.querySelector(".note-text");
+      const textEl = newNote.querySelector('.note-text');
       if (note.params) {
         console.log('bum');
         this.#createAside(asideEl, note, textEl);
@@ -21,29 +21,54 @@ export class NoteView {
 
   #createAside(asideEl, note, textEl) {
     const newAside = document.importNode(asideEl, true);
-    const parentEl = newAside.querySelector("p");
+    const parentEl = newAside.querySelector('p');
     let j = 1;
-    const detail = note.params.map(
-      (param, i) => {
+    const detail = note.params
+      .map((param, i) => {
         const detail = `<mark class="mark-${j}">${param?.desc}</mark>`;
         j++;
-        if (i > 0 && i % 6 === 0)
-          j = 1;
+        if (i > 0 && i % 6 === 0) j = 1;
         return detail;
-      }
-    )
-      .join("<br>");
+      })
+      .join('<br>');
     parentEl.innerHTML = detail;
     textEl.appendChild(newAside);
   }
 
   #createNote(newNote, noteEl, note) {
     newNote = document.importNode(noteEl, true);
-    const titleEl = newNote.querySelector(".note-title");
-    titleEl.textContent = titleEl.textContent.replace(/{%TITLE%}/g, `${note.title}\n`);
-    const noteTextEl = newNote.querySelector(".note-note");
-    noteTextEl.innerHTML = noteTextEl.textContent.replace(/{%NOTE%}/g, note.note.join('\n<br>'));
+    const titleEl = newNote.querySelector('.note-title');
+    titleEl.textContent = titleEl.textContent.replace(
+      /{%TITLE%}/g,
+      `${note.title}\n`
+    );
+    const noteTextEl = newNote.querySelector('.note-note');
+    if (note.params) {
+      noteTextEl.innerHTML = noteTextEl.textContent.replace(
+        /{%NOTE%}/g,
+        this.#insertParams(note.note.join('\n<br>'), note.params)
+      );
+    } else {
+      noteTextEl.innerHTML = noteTextEl.textContent.replace(
+        /{%NOTE%}/g,
+        note.note.join('\n<br>')
+      );
+    }
     return newNote;
+  }
+
+  #insertParams(text, params) {
+    return text.format(...this.#getParams(params));
+  }
+
+  #getParams(params) {
+    let j = 1;
+    return params.map((param, i) => {
+      const mark = `<mark class="mark-${j}">${param?.name}</mark>`;
+      j++;
+      if (i > 0 && i % 6 === 0) j = 1;
+      return mark;
+    });
   }
 }
 
