2ec5d5df2ab505ea1a683a24f234cb9b7008e607
atari-monk <atari.monk1@gmail.com>
2023-03-30 16:28:54 +0200

    upd orientation test

--- my-templates/screen-orientation/script.js
+++ my-templates/screen-orientation/script.js
@@ -1,4 +1,9 @@
 screen.addEventListener('orientationchange', () => {
   console.log(`The orientation of the screen is: ${screen.orientation}`);
+  const testEl = document.querySelector('#test');
+  testEl.innerHTML = `The orientation of the screen is: ${screen.orientation}`;
 });
-console.log('test');
+
+//document.addEventListener('load')
+const testEl = document.querySelector('#test');
+testEl.innerHTML = `The orientation of the screen is: ${screen.orientation}`;
